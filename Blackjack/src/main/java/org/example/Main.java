2533202abf950363b4d5adcf363f3745dac7d5e9
ConnorThimot <connorthimot97@gmail.com>
2026-04-16 18:08:28 -0300

    Delete Blackjack directory

--- Blackjack/src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Blackjack!");
-
-        Game blackjack = new Game();
-    }
-}
